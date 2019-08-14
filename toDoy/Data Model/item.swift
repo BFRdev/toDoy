9e32eb6925ccba2b67dc98cac8ae4d5db8d0f073
Ameer <ameeryasin17@gmail.com>
2019-08-14 12:25:38 -0400

    convert to MCV

---
+++ toDoy/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  toDoy
+//
+//  Created by Ameer Yasin on 2019-08-12.
+//  Copyright © 2019 Ameer Yasin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
